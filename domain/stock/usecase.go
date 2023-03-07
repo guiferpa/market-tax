ab82d7609330348ac33f391faaadf84975400f86
Guilherme Paixão <guiferpa@gmail.com>
2023-03-06 23:00:00 -0300

    feat: created logic to calc weighted average

--- domain/stock/usecase.go
+++ domain/stock/usecase.go
@@ -6,6 +6,17 @@ func calcTax(gain int) int {
 	return (gain * 20) / 100
 }
 
+type calcWeightedAverageParams struct {
+	CurrentStockQuantity   int
+	StockBoughtQuantity    int
+	CurrentWeightedAverage int
+	BoughtStockUnitCost    int
+}
+
+func calcWeightedAverage(p calcWeightedAverageParams) int {
+	return ((p.CurrentStockQuantity * p.CurrentWeightedAverage) + (p.StockBoughtQuantity * p.BoughtStockUnitCost)) / (p.CurrentStockQuantity + p.StockBoughtQuantity)
+}
+
 func NewUseCaseService() *UseCaseService {
 	return &UseCaseService{}
 }
